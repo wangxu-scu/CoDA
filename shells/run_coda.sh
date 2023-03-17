57506f5649a744bd91d0ef482800574b469c3216
wangxu-scu <33362972+wangxu-scu@users.noreply.github.com>
2023-03-17 10:50:06 +0800

    Update run_coda.sh

--- shells/run_coda.sh
+++ shells/run_coda.sh
@@ -1,8 +1,6 @@
 cd ../
-# dataset='office31'; class_num=31; cluster_num=50; tasks='w2d' #a2w d2a d2w w2a w2d'
-# dataset='image_CLEF'; class_num=12; cluster_num=12; tasks='b2c b2i b2p c2b c2i c2p i2b i2c i2p p2b p2c p2i'
 dataset='office_home'; class_num=65; cluster_num=100; tasks='a2c' #a2c a2p p2r #a2c a2p a2r c2p c2r p2r' #c2r c2p p2r' #'a2c a2p a2r c2a c2p c2r p2a p2c p2r r2a r2c r2p'
-# dataset='adaptiope'; class_num=123; cluster_num=100; tasks='r2s' #p2s r2p r2s s2p s2r'
+
 
 
 gpu_id=0
